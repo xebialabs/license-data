8319b446c6d776123ba4b8a4faa5ef2d1211c372
Jasper Stein <jstein@xebialabs.com>
2018-04-19 09:59:57 +0200

    Allow update of multiple branches

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,6 +7,13 @@ def releaseArgs(params) {
         "-Prelease.explicit=${params.RELEASE_EXPLICIT}"
 }
 
+def supportedBranches = [
+    '7.0.x-maintenance',
+    '7.5.x-maintenance',
+    '8.0.x-maintenance',
+    'master'
+]
+
 pipeline {
   agent none
 
@@ -25,6 +32,7 @@ pipeline {
   parameters {
     choice(name: 'RELEASE_SCOPE', choices: 'patch\nminor\nmajor', description: 'Which version component should be incremented?')
     string(name: 'RELEASE_EXPLICIT', defaultValue: '', description: 'In case of a new development cycle you may need to set the version number explicitly if it is non-contiguous. E.g. put something like 1.2.3 or 1.2.3-beta.10 here.')
+    string(name: 'PUSHABLE_BRANCHES', defaultValue: supportedBranches.join(' '), description: 'a space-separated list of branch names that will be updated')
   }
 
   stages {
@@ -49,15 +57,17 @@ pipeline {
     }
 
     stage('Run Update dependencies') {
-      steps {
-        build job: "Update dependencies",
-            parameters: [
-                string(name: 'branch', value: 'master'),
-                string(name: 'project', value: 'groupUpdateAllDependencies'),
-                string(name: 'dependency', value: 'licenseDatabaseVersion'),
-                string(name: 'newValue', value: newVersion)
-            ],
-            wait: false
+      parallel {
+        params.PUSHABLE_BRANCHES.split('\\w').collect { branch ->
+            build job: "Update dependencies on branch $branch",
+                parameters: [
+                    string(name: 'branch', value: branch),
+                    string(name: 'project', value: 'groupUpdateAllDependencies'),
+                    string(name: 'dependency', value: 'licenseDatabaseVersion'),
+                    string(name: 'newValue', value: newVersion)
+                ],
+                wait: false
+        }
       }
     }
   }
