5bc9257bb762327f429c8ab0619d74584f963b43
Jasper Stein <jstein@xebialabs.com>
2019-11-12 15:22:38 +0100

    Generate Excel-consumable .csv files for easy conversion to .xlsx

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ The following command can be used to generate a CSV file with libraries and lice
 Bart will reach out to generate new overviews of libraries and their licenses which are included in our products. Using a script these can be generated from an unzipped distribution.
 
     $ python3 generate-list.py <XLD_SERVER_HOME>/lib > output.txt
+
+# Generating an Excel file for our legal department
+
+On 11 and 12 november 2019 in an email chain involving me (Jasper Stein), Mayur Patel, Benjamin Rosen, Martin O'Keefe, Christian
+van den Branden, Mike McKechnie it was determined that we want an `.xlsx` sheet with an overview of OSS licenses we use, on each
+version of XLD and XLR we release. To aid the creation of this some more, the following script will produce a `.csv` file that
+Microsoft Excel will know how to parse, so it is easy to let it turn it into an `.xlsx` file for upload to, e.g., 
+https://xebialabs.app.box.com/folder/86224075625 for XL Deploy or https://xebialabs.app.box.com/folder/86223755194 for XL Release
+
+    $ python3 generate-csv.py <XLD_SERVER_HOME>/lib <outputfile>
---
+++ generate-csv.py
@@ -0,0 +1,44 @@
+import json
+from collections import defaultdict
+from os import listdir
+from os.path import isfile, isdir, join
+import sys
+
+lib_version = {}
+
+if len(sys.argv) < 3 or not isdir(sys.argv[1]):
+    print("Usage 'python %s <lib directory of XL tool> <destination file>'" % sys.argv[0])
+    exit(1)
+
+for f in [f for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))]:
+    lib_name = '.'.join(f.strip().split('.')[0:-1])
+    parts = lib_name.strip().split('-')
+    lib_version['-'.join(parts[0:-1])] = parts[-1]
+
+art_ga = defaultdict(list)
+ga_license = defaultdict(list)
+
+with open('src/main/resources/license-data.json', 'r') as f:
+    d = json.load(f)
+    arts = d['artifacts']
+    for k, v in arts.items():
+        a = k.split(':')[1] if ':' in k else k
+        if a in art_ga:
+            print("WARN: DUPLICATE '%s' FOUND for %s and %s" % (a, k, art_ga[a]), file=sys.stderr)
+        art_ga[a].append(k)
+        ga_license[k].append(v["license"])
+        if 'additionalLicenses' in v:
+            additionalLicenses = v['additionalLicenses']
+            if type(additionalLicenses) is list:
+                for l in additionalLicenses:
+                    ga_license[k].append(l)
+            else:
+                ga_license[k].append(additionalLicenses)
+
+with open(sys.argv[2], 'w') as s:
+    for lib in sorted(lib_version.keys()):
+        version = lib_version[lib]
+        group_artifacts = art_ga[lib]
+        for ga in group_artifacts:
+            licenses = ga_license[ga]
+            s.write("%s;%s;%s\r\n" % (ga.replace(":",";"), version, ','.join(licenses)))
